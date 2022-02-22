e804bcf2af17df84aa7026aea43f8da1aa6db0fa
Jasaholic <100187386+jasaholic@users.noreply.github.com>
2022-02-22 06:16:55 -0800

    Pengaturan Koneksi database

--- lib/config.php
+++ lib/config.php
@@ -1,8 +1,8 @@
 <?php
 //Config Database
 date_default_timezone_set('Asia/Jakarta');
-$db_server = "localhost";
-$db_user = "bozztool_home";
-$db_password = "Sadiwantoro95";
-$db_name = "bozztool_home";
-$cfg_baseurl = "https://bozztool.com";
+$db_server = "localhost"; //server
+$db_user = ""; //username database
+$db_password = ""; //sandi database
+$db_name = ""; //nama database
+$cfg_baseurl = "nama_domain.com"; //link website. jangan gunakan akhiran /
